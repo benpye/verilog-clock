6b63c1d7e55c68e25d50733481ba83b8ee037713
Ben Pye <ben@curlybracket.co.uk>
2014-08-21 19:26:16 +0100

    Rewritten main logic

--- clock.v
+++ clock.v
@@ -1,103 +1,85 @@
 module clock(
 		// Physical IO devices
-		input [0:0] CLOCK_24,
-		input [1:0] KEY,
-		output reg [6:0] HEX0,
-		output reg [6:0] HEX1,
-		output reg [6:0] HEX2,
-		output reg [6:0] HEX3,
-		output reg [13:0] GPIO_0,
-		// Use as AM/PM indicator
-		output reg [0:0] LEDR
+		input  [0:0] CLOCK_24,
+		input  [1:0] KEY,
+		output [6:0] HEX0,
+		output [6:0] HEX1,
+		output [6:0] HEX2,
+		output [6:0] HEX3,
+		output [13:0] GPIO_0
 	);
 	
 	// Time keeping
-	reg [24:0] int_count;
-	reg [5:0] seconds;
-	reg [5:0] minutes;
-	reg [4:0] hours;
-	reg [4:0] hours_draw;
-	reg [2:0] state;
-	reg [3:0] param [0:0];
+	reg  [24:0] int_count;
+	reg  [5:0] seconds;
+	reg  [5:0] minutes;
+	reg  [4:0] hours;
+	reg  [5:0] days;
+	reg  [4:0] months;
+	reg  [6:0] years;
 	
-	// Input
+	// BCD decoding
+	reg  [7:0] bcd_input;
+	wire [3:0] bcd_output_ones;
+	wire [3:0] bcd_output_tens;
+	wire [3:0] bcd_output_hundreds;
+	
+	bcd bcd_decoder(
+		.value( bcd_input ),
+		.hundreds( bcd_output_hundreds ),
+		.tens( bcd_output_tens ),
+		.ones( bcd_output_ones )
+	);
+	
+	// 7 segment decoding
+	reg  [3:0] ss_input;
+	wire [6:0] ss_output;
+	
+	seven_segment ss_decoder(
+		.value( ss_input ),
+		.display( ss_output )
+	);
+	
+	// Display output buffer
+	reg  [3:0] display_counter;
+	reg  [6:0] display_buffer [0:5];
+	assign GPIO_0[13:7] = display_buffer[0];
+	assign GPIO_0[6:0]  = display_buffer[1];
+	assign HEX0         = display_buffer[2];
+	assign HEX1         = display_buffer[3];
+	assign HEX2         = display_buffer[4];
+	assign HEX3         = display_buffer[5];
+	
+	// State machine
+	reg  [3:0] set_state;
+	
+	// User input
 	reg key_ff1 [0:1];
 	reg key_ff2 [0:1];
 	reg key_reg [0:1];
 	
-	// Display from clock
-	reg [3:0] val_disp [0:5];
-	
-	wire [6:0] clock_display [0:5];
-	
-	wire [3:0] second_ones;
-	wire [3:0] second_tens;
-	
-	wire [3:0] minute_ones;
-	wire [3:0] minute_tens;
-	
-	wire [3:0] hour_ones;
-	wire [3:0] hour_tens;
-	
-	bcd bcd_seconds(
-		.value( seconds ),
-		.tens( second_tens ),
-		.ones( second_ones )
-	);
-	
-	seven_segment ss_disp0(
-		.value( val_disp[0] ),
-		.display( clock_display[0] )
-	);
-	
-	seven_segment ss_disp1(
-		.value( val_disp[1] ),
-		.display( clock_display[1] ) );
-		
-	bcd bcd_minutes(
-		.value( minutes ),
-		.tens( minute_tens ),
-		.ones( minute_ones )
-	);
-	
-	seven_segment ss_disp2(
-		.value( val_disp[2] ),
-		.display( clock_display[2] )
-	);
-	
-	seven_segment ss_disp3(
-		.value( val_disp[3] ),
-		.display( clock_display[3] ) );
-		
-	bcd bcd_hours(
-		.value( hours_draw ),
-		.tens( hour_tens ),
-		.ones( hour_ones )
-	);
-	
-	seven_segment ss_disp4(
-		.value( val_disp[4] ),
-		.display( clock_display[4] )
-	);
-	
-	seven_segment ss_disp5(
-		.value( val_disp[5] ),
-		.display( clock_display[5] ) );
-	
 	// Setup the initial values
 	initial begin
+		// Timing initial values
 		int_count = 0;
 		seconds = 0;
 		minutes = 0;
 		hours = 0;
-		state = 0;
 		
-		// Default settings
-		param[0] = 0;
+		days = 1;
+		months = 1;
+		years = 0;
+		
+		// Initial state
+		set_state = 0;
+		
+		// Display initial values
+		display_counter = 0;
 	end
 	
 	integer i;
 	
+	// Metastability fix for the user input
 	always @( posedge CLOCK_24[0] ) begin
 		for( i=1; i>=0; i=i-1 ) begin
 			key_ff1[i] <= !KEY[i];
@@ -106,58 +88,7 @@ module clock(
 	end
 	
 	always @( posedge CLOCK_24[0] ) begin
-		if( key_ff2[0] && !key_reg[0] ) begin
-			// If we are in the clock state AND we are holding the other button
-			if( state == 0 && key_ff2[1] ) begin
-				// We will then enter the other settings - eg 12/24 hour
-				state = 4;
-			end
-			else begin
-				// Otherwise stay in normal states
-				state = state + 1;
-				// State == 4 is end of time set, == 5 is end of param set
-				if( state == 4 || state == 5 ) begin
-					state = 0;
-				end
-			end
-			key_reg[0] = 1;
-		end
-		
-		if( key_ff2[1] && !key_reg[1] ) begin
-			case( state )
-				1: begin
-					hours = hours + 1;
-					if( hours == 24 ) begin
-						hours = 0;
-					end
-				end
-				2: begin
-					minutes = minutes + 1;
-					if( minutes == 60 ) begin
-						minutes = 0;
-					end
-				end
-				3: begin
-					seconds = 0;
-					int_count = 0;
-				end
-				4: begin
-					param[0] = param[0] + 1;
-					if( param[0] == 2 ) begin
-						param[0] = 0;
-					end
-				end
-			endcase
-			
-			key_reg[1] = 1;
-		end
-		
-		for( i=1; i>=0; i=i-1 ) begin
-			if( !key_ff2[i] ) begin
-				key_reg[i] = 0;
-			end
-		end
-		
+		// Time keeping
 		int_count = int_count + 1;
 		
 		// Divide 24MHz by 24000000 to get 1Hz
@@ -177,70 +108,135 @@ module clock(
 			
 			if( hours == 24 ) begin
 				hours = 0;
+				days = days + 1;
+			end
+			
+			// If we're in a set mode allow the day to go to 31 for any month, this lets the user set the date to 29 feb etc.
+			if( ( months == 4 || months == 6 || months == 9 || months == 11 ) && days == 31 && set_state == 0 ) begin
+				months = months + 1;
+				days = 1;
+			end
+			else if( months == 2 && years[1:0] == 0 && days == 30 && set_state == 0 ) begin
+				months = months + 1;
+				days = 1;
+			end
+			else if( months == 2 && years[1:0] != 0 && days == 29 && set_state == 0 ) begin
+				months = months + 1;
+				days = 1;
+			end
+			else if( days == 32 ) begin
+				months = months + 1;
+				days = 1;				
+			end
+			
+			if( months == 13 ) begin
+				months = 1;
+				years = years + 1;
 			end
 		end
 		
-		// These are clock display like states - eg time, set hour/minute/second
-		if( state < 4 ) begin
-			hours_draw = hours;
-			LEDR[0] = 0;
-			
-			if( param[0] && hours >= 12 ) begin
-				hours_draw = hours - 12;
-				LEDR[0] = 1;
-			end
-			
-			if( param[0] && hours_draw == 0 ) begin
-				hours_draw = 12;
-			end
+		// User input
 		
-			// In clock mode all displays are just that of the BCD output
-			val_disp[0] = second_ones;
-			val_disp[1] = second_tens;
-			val_disp[2] = minute_ones;
-			val_disp[3] = minute_tens;
-			val_disp[4] = hour_ones;
-			val_disp[5] = hour_tens;
+		// Set button - changes state
+		if( key_ff2[0] && !key_reg[0] ) begin
+			set_state = set_state + 1;
 			
-			// And all go straight to the displays
-			GPIO_0[13:7] = clock_display[0];
-			GPIO_0[6:0] = clock_display[1];
+			// States wrap around at max + 1
+			if( set_state == 7 )
+				set_state = 0;
 			
-			HEX0 = clock_display[2];
-			HEX1 = clock_display[3];
-			HEX2 = clock_display[4];
-			HEX3 = clock_display[5];
+			key_reg[0] = 1;
 		end
-		else if( state >= 4 ) begin
-			// Parameter states - display paramater number and value
-			val_disp[0] = param[state - 4];
-			val_disp[5] = state - 4;
+		
+		// "Mode" button - changes values
+		if( key_ff2[1] && !key_reg[1] ) begin
+			case( set_state )
+				1: begin
+					hours = hours + 1;
+					if( hours == 24 )
+						hours = 0;
+				end
+				2: begin
+					minutes = minutes + 1;
+					if( minutes == 60 )
+						minutes = 0;
+				end
+				3: begin
+					seconds = 0;
+					int_count = 0;
+				end
+				4: begin
+					days = days + 1;
+					if( days == 32 )
+						days = 1;
+				end
+				5: begin
+					months = months + 1;
+					if( months == 13 )
+						months = 1;
+				end
+				6: begin
+					years = years + 1;
+					if( years == 100 )
+						years = 0;
+				end
+			endcase
 			
-			GPIO_0[13:7] = clock_display[0];
-			HEX3 = clock_display[5];
-			
-			GPIO_0[6:0] = 7'b1111111;
-			HEX0 = 7'b1111111;
-			HEX1 = 7'b1111111;
-			HEX2 = 7'b1111111;
+			key_reg[1] = 1;
 		end
+		
+		for( i=1; i>=0; i=i-1 ) begin
+			if( !key_ff2[i] )
+				key_reg[i] = 0;
+		end
+		
+		// BCD management
+		if( key_ff2[1] && set_state == 0 || set_state > 3 ) begin
+			// Display date when holding mode or setting date element
+			case( display_counter[3:1] )
+				0: bcd_input = months;
+				1: bcd_input = days;
+				2: bcd_input = years;
+			endcase
+		end
+		else begin
+			// Display time
+			case( display_counter[3:1] )
+				0: bcd_input = minutes;
+				1: bcd_input = hours;
+				2: bcd_input = seconds;
+			endcase
+		end
+		
+		// 7 Segment management
+		if( display_counter[0] )
+			ss_input = bcd_output_ones;
+		else
+			ss_input = bcd_output_tens;
+		
+		display_buffer[display_counter] = ss_output;
 		
 		// Flashing display for setting the correct values
 		if( int_count[22] == 0 ) begin
-			case( state )
-				1: begin
-					HEX2 = 7'b1111111;
-					HEX3 = 7'b1111111;
+			case( set_state )
+				// Hours - minutes - seconds
+				1,4: begin
+					display_buffer[5] = 7'b1111111;
+					display_buffer[4] = 7'b1111111;
 				end
-				2: begin
-					HEX0 = 7'b1111111;
-					HEX1 = 7'b1111111;
+				2,5: begin
+					display_buffer[3] = 7'b1111111;
+					display_buffer[2] = 7'b1111111;
 				end
-				3: begin
-					GPIO_0[13:7] = 7'b1111111;
-					GPIO_0[6:0] = 7'b1111111;
+				3,6: begin
+					display_buffer[1] = 7'b1111111;
+					display_buffer[0] = 7'b1111111;
 				end
 			endcase
 		end
+		
+		display_counter = display_counter + 1;
+		if( display_counter == 6 )
+			display_counter = 0;
 	end
 endmodule
--- seven_segment.v
+++ seven_segment.v
@@ -1,21 +1,21 @@
 module seven_segment(
-		input [7:0] value,
+		input      [3:0] value,
 		output reg [6:0] display
 	);
 	
 	always @( value ) begin
 		case( value )
-			0: display = 7'b1000000;
-			1: display = 7'b1001111;
-			2: display = 7'b0100100;
-			3: display = 7'b0110000;
-			4: display = 7'b0011001;
-			5: display = 7'b0010010;
-			6: display = 7'b0000010;
-			7: display = 7'b1111000;
-			8: display = 7'b0000000;
-			9: display = 7'b0011000;
-			default: display = 7'b1111111;
+			0: display <= 7'b1000000;
+			1: display <= 7'b1001111;
+			2: display <= 7'b0100100;
+			3: display <= 7'b0110000;
+			4: display <= 7'b0011001;
+			5: display <= 7'b0010010;
+			6: display <= 7'b0000010;
+			7: display <= 7'b1111000;
+			8: display <= 7'b0000000;
+			9: display <= 7'b0011000;
+			default: display <= 7'b1111111;
 		endcase
 	end
 endmodule
